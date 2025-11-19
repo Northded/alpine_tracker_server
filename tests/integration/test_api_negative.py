bb1726a9c802348d3519ad0891112d6adfd76b6b
oleg <famouse2016@gmail.com>
2025-11-19 20:22:46 +0300

    feat: del neg test

--- tests/integration/test_api_negative.py
+++ tests/integration/test_api_negative.py
@@ -1,24 +1,24 @@
-import pytest
+# import pytest
 
-pytestmark = pytest.mark.integration
+# pytestmark = pytest.mark.integration
 
 
-class TestValidation:
-    def test_mountain_height_must_be_positive(self, client):
-        """
-        Критический тест: высота горы должна быть положительной.
-        """
-        # Попытка создать гору с отрицательной высотой
-        invalid_data = {
-            "name": "Invalid Mountain",
-            "height": -1000,  
-            "location": "Test Location",
-            "difficulty": "Низкая"
-        }
+# class TestValidation:
+#     def test_mountain_height_must_be_positive(self, client):
+#         """
+#         Критический тест: высота горы должна быть положительной.
+#         """
+#         # Попытка создать гору с отрицательной высотой
+#         invalid_data = {
+#             "name": "Invalid Mountain",
+#             "height": -1000,  
+#             "location": "Test Location",
+#             "difficulty": "Низкая"
+#         }
         
-        response = client.post("/mountains/", json=invalid_data)
+#         response = client.post("/mountains/", json=invalid_data)
         
-        assert response.status_code == 422, \
-            f"ОШИБКА! Сервер принял отрицательную высоту! " \
-            f"Получен код {response.status_code} вместо 422. " \
-            f"Валидация не работает!"
+#         assert response.status_code == 422, \
+#             f"ОШИБКА! Сервер принял отрицательную высоту! " \
+#             f"Получен код {response.status_code} вместо 422. " \
+#             f"Валидация не работает!"
